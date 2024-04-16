c2832ccd89e64e3272cf242c0fd9b76ace542d0b
ajax-bakun-n <bakun.n@ajax.systems>
2024-04-16 22:17:10 +0300

    Add buildfile for Windows

---
+++ build.ps1
@@ -0,0 +1,5 @@
+mvn clean package #java package
+#docker image build -t jakarta-project:latest . #build the image
+#docker container run jakarta-project:latest #execute as container
+cd docker
+docker-compose up --build
